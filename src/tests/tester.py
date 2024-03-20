293f21b7cfcd311d2756c24ecbc4d3844d63152d
Kornilovich Mikhail <kornilovichmihail@gmail.com>
2024-03-20 21:29:21 +0300

    ok

--- src/tests/tester.py
+++ src/tests/tester.py
@@ -1,13 +1,17 @@
 from abc import ABC, abstractmethod
+from typing import Tuple, List, Any
 
 import numpy as np
+from matplotlib import pyplot as plt
 from matplotlib.animation import Animation
+from scipy.optimize import minimize
 
-from ..common import Point
+from ..common import Point, LambdaOracul, PointFigure
 from ..common.method import OptimizationMethod, Oracul
 from ..lab1.method_processor import MethodProcessor
 from ..lab1.stop_condition import PrecisionCondition
 from ..metric import CallCount, GradientCount, StepCountBeforePrecision
+from ..visualization import Animator
 
 
 class Tester(ABC):
@@ -54,3 +58,35 @@ class SimpleTester(Tester):
             points.append([part_points])
             metrics.append([part_metrics])
         return points, metrics, animations
+
+
+class ScipyTester(Tester):
+    def __init__(self, methods: [str], oraculs: list[tuple[LambdaOracul, LambdaOracul]],
+                 start_point: Point = Point(np.array([100, 200])), eps: float = 1e-3):
+        self.methods = methods
+        self.oraculs = oraculs
+        self.start_point = start_point
+        self.eps = eps
+
+    def test(self) -> tuple[list[list[list[list[Point]]]], list[list[list[list[Any]]]], list[None]]:
+        points = []
+        metrics = []
+        for method in self.methods:
+            part_points = []
+            part_metrics = []
+            for oracul in self.oraculs:
+                plt.figure()
+                res = minimize(method="Nelder-Mead", fun=oracul[0].func, x0=self.start_point.coordinates,
+                               options={"return_all": True, "disp": True, "xatol": True, "fatol": True}, tol=self.eps)
+                point = res.x
+                figures = []
+                for point in res.allvecs:
+                    figures.append(PointFigure([*point, oracul[1].evaluate(Point(point))]))
+                Animator.animate([figures], oracul[1], oracul[1].get_dimension(), step=0.01, start=[-100, -100],
+                                 end=[200, 200])
+                plt.show()
+                part_points.append([Point(point)])
+                part_metrics.append([res.nfev])
+            points.append([part_points])
+            metrics.append([part_metrics])
+        return points, metrics, [None]
